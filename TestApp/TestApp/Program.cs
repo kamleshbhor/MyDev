524f6da6af49097a3bb11b30ca56ded9934342a4
Kamlesh.Bhor <Kamlesh.Bhor@SAGITEC.com>
2016-12-30 11:40:28 +0530

    Added new solution

---
+++ TestApp/TestApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintName();
+            Console.Read();
+        }
+
+        public static void PrintName()
+        {
+            Console.WriteLine("Kamlesh");
+        }
+    }
+}
